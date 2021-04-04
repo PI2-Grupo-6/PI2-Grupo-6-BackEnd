9d55a68f46698ebf935f597e19cfb61f59a25cb5
Mateus Nóbrega <mateus@nobrega.dev>
2021-04-04 01:38:23 -0300

    Fix mongo database connection link

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import mongoose, { mongo } from 'mongoose';
 
 const app = express();
 const port = 3000;
@@ -7,7 +8,16 @@ app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
 
+mongoose.connect('mongodb://mongo:27017/backend', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
 
 app.listen(port, () => {
 	console.log(`App listening at http://localhost:${port}`);
+
+	const db = mongoose.connection;
+	db.on('error', console.error.bind(console, 'Connection error: '));
+	db.once('open', () => console.log('Connected to MongoDB'));
 });
+
