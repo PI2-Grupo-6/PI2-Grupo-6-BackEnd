e3d8c2e3009118f4c5d0ec229980f3af8c4695ee
Mateus Nóbrega <mateus@nobrega.dev>
2021-05-16 19:31:53 -0300

    Register routing

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,15 @@ import express from 'express';
 import foodRoutes from './routes/foodRoutes';
 import orderRoutes from './routes/orderRoutes';
 import menuRoutes from './routes/menuRoutes';
+import machineRoutes from './routes/machineRoutes';
+import restaurantRoutes from './routes/restaurantRoutes';
 
 const routes = express.Router();
 
 routes.use('/foods', foodRoutes);
-routes.use('/orders', orderRoutes);
+routes.use('/machines', machineRoutes);
 routes.use('/menu', menuRoutes);
+routes.use('/orders', orderRoutes);
+routes.use('/restaurants', restaurantRoutes)
 
 export default routes;
